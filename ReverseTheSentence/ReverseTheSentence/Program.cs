30e091f1d501b246120bcfe2225e47a7a8eee3a5
Gudbrandr42 <mark.bullard42@gmail.com>
2020-01-22 13:49:28 -0500

    added content to Program.cs for demo

--- ReverseTheSentence/ReverseTheSentence/Program.cs
+++ ReverseTheSentence/ReverseTheSentence/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace ReverseTheSentence
 {
-    class Program
+    static class Words
     {
-        static void Main(string[] args)
+        public static string Sentence(string sentence)
         {
+            string[] words = sentence.Split();
+            Array.Reverse(words);
+            return string.Join("", words);
+        }
+    }
+    class Application
+    {
+        static void Main()
+        {
+            const string SumSentence = "I want to be reversed!";
+            string sentence = Words.Sentence(SumSentence);
+            Console.WriteLine(sentence);
         }
     }
 }
